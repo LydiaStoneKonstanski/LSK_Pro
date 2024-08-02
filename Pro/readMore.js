5de748efdf0c6a877e784892dd3877772c23a859
LydiaStoneKonstanski <lkonstanski@yahoo.com>
2024-08-02 16:56:27 -0400

    Icons for the third approach to resume, and Read More button with a drop-down. I don't love where it is sitting, but I think it's a great approach to the problem of having too much text density, and allowing a user to opt out of it.

--- Pro/readMore.js
+++ Pro/readMore.js
@@ -1,11 +1,11 @@
 // When the user clicks on the button,
 //toggle between hiding and showing the dropdown content
 function menuFunction() {
-    document.getElementById("burgerDropdown").classList.toggle("show");
+    document.getElementById("read-more-qual").classList.toggle("show");
 }
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.material-symbols-outlined')) {
+    if (!event.target.matches('.btn-primary')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
